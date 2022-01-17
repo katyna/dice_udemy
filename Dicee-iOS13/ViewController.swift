ef44df53b19ba048a4fe90c620ed3cb94c8ae121
Nazakat <nazakat@Mieraidihaimus-MacBook-Pro-2.local>
2022-01-17 17:37:45 +0000

    add initial change

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+
+    }
+
+}
+
